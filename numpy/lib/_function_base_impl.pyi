af62366be9c667c10fff0c68bdf494a961fe1579
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2024-09-27 13:08:13 +0200

    MAINT: Apply ruff/pycodestyle rule E252
    E252 Missing whitespace around parameter equals

--- numpy/lib/_function_base_impl.pyi
+++ numpy/lib/_function_base_impl.pyi
@@ -99,7 +99,7 @@ def iterable(y: object) -> TypeGuard[Iterable[Any]]: ...
 def average(
     a: _ArrayLikeFloat_co,
     axis: None = ...,
-    weights: None | _ArrayLikeFloat_co= ...,
+    weights: None | _ArrayLikeFloat_co = ...,
     returned: L[False] = ...,
     keepdims: L[False] = ...,
 ) -> floating[Any]: ...
@@ -123,7 +123,7 @@ def average(
 def average(
     a: _ArrayLikeFloat_co,
     axis: None = ...,
-    weights: None | _ArrayLikeFloat_co= ...,
+    weights: None | _ArrayLikeFloat_co = ...,
     returned: L[True] = ...,
     keepdims: L[False] = ...,
 ) -> _2Tuple[floating[Any]]: ...
